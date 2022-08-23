ac0e9aec20251086bef5066e46f2cc481fab51bb
Vaibhav Joshi <vaibhavj478@gmail.com>
2022-08-23 13:33:11 +0530

    add pricing

--- src/Pages/day1/ForPricing/ForPricingPage.tsx
+++ src/Pages/day1/ForPricing/ForPricingPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 const ForPricing = (props: Props) => {
-	return <div>ForPricing</div>;
+	return <div>ForPricing vaibhav joshi</div>;
 };
 
 export default ForPricing;
