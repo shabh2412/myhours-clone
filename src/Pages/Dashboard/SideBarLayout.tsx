7aafe0857c5a691819863f90b3b5e45be790e146
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-25 16:50:38 +0530

    created sidebar layout

---
+++ src/Pages/Dashboard/SideBarLayout.tsx
@@ -0,0 +1,46 @@
+import {
+	Box,
+	Drawer,
+	DrawerContent,
+	DrawerOverlay,
+	useColorModeValue,
+	useDisclosure,
+} from "@chakra-ui/react";
+import { SidebarContent } from "../../Components/Dashboard/SideBar/SideBarContent";
+import React from "react";
+
+export const SideBarLayout = ({ children }: { children?: React.ReactNode }) => {
+	const sidebar = useDisclosure();
+
+	// const color = useColorModeValue("gray.600", "gray.300");
+	return (
+		<Box
+			as="section"
+			bg="gray.50"
+			_dark={{
+				bg: "gray.700",
+			}}
+			minH="100vh">
+			<SidebarContent />
+			<Drawer
+				isOpen={sidebar.isOpen}
+				onClose={sidebar.onClose}
+				placement="left">
+				<DrawerOverlay />
+				<DrawerContent>
+					<SidebarContent w="full" borderRight="none" />
+				</DrawerContent>
+			</Drawer>
+			<Box
+				ml={{
+					base: 0,
+					md: 60,
+				}}>
+				<Box as="main" p="1">
+					{/* Add content here, remove div below  */}
+					{children}
+				</Box>
+			</Box>
+		</Box>
+	);
+};
