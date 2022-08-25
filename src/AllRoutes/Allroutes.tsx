db5619a7777587debf70bc93f4987092a68b0a42
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-25 16:53:54 +0530

    created nested routes for dashboard sections

--- src/AllRoutes/Allroutes.tsx
+++ src/AllRoutes/Allroutes.tsx
@@ -1,9 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import SectionHeader from "../Components/Dashboard/SectionHeaders/SectionHeader";
+import { SidebarContent } from "../Components/Dashboard/SideBar/SideBarContent";
 import Footer from "../Components/ForFooter/Footer";
 import Navbar from "../Components/ForNavbar/Navbar";
+import Clients from "../Pages/Dashboard/Clients";
 import Dashboard from "../Pages/Dashboard/Dashboard";
+import { SideBarLayout } from "../Pages/Dashboard/SideBarLayout";
+import TeamMembers from "../Pages/Dashboard/TeamMembers";
 import ForHowItWorks from "../Pages/day1/ForHowItWorks/ForHowItWorks";
 import ForLandingPage from "../Pages/day1/ForLandingPage/ForLandingPage";
 import FirebasePhone from "../Pages/day1/ForRegistration/ForAuth/FirebasePhone";
@@ -15,7 +20,6 @@ const Allroutes = () => {
 	return (
 		<Box>
 			<Routes>
-				<Route path="/dashboard" element={<Dashboard />} />
 				<Route path="/login" element={<LoginPage />}></Route>
 				<Route path="/register" element={<Register />}></Route>
 				<Route
@@ -57,6 +61,10 @@ const Allroutes = () => {
 						</Box>
 					}
 				/>
+				<Route path="dashboard" element={<Dashboard />}>
+					<Route path="team-members" element={<TeamMembers />} />
+					<Route path="clients" element={<Clients />} />
+				</Route>
 			</Routes>
 		</Box>
 	);
