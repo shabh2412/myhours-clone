480f04f22c6e9fb13add42919c35cd16a0625a7c
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-27 04:30:18 +0530

    completed with client page, except the edit function

--- src/Pages/Dashboard/Clients.tsx
+++ src/Pages/Dashboard/Clients.tsx
@@ -1,14 +1,18 @@
+import { Box, HStack, Progress, Text } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import LoadingComponent from "../../Components/Dashboard/LoadingComponent";
 import SectionHeader from "../../Components/Dashboard/SectionHeaders/SectionHeader";
-import { getClients } from "../../Store/Clients/action";
+import { deleteClient, getClients } from "../../Store/Clients/action";
 import { RootReducer } from "../../Store/store";
 import DataTable from "./Components/Table";
 
 type Props = {};
 
 const Clients = (props: Props) => {
-	const { data } = useSelector((state: RootReducer) => state.clients);
+	const { data, _delete, get } = useSelector(
+		(state: RootReducer) => state.clients
+	);
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch(getClients());
@@ -25,7 +29,17 @@ const Clients = (props: Props) => {
 				formLink="new"
 			/>
 			{/* Add table component after merging. */}
-			{data.length > 0 && <DataTable tableData={data} />}
+			{_delete.loading && <LoadingComponent message="Deleting Data..." />}
+			{get.loading && <LoadingComponent message="Loading Data..." />}
+			{data.length > 0 ? (
+				<DataTable tableData={data} deleteHandler={deleteClient} />
+			) : (
+				<HStack h="80vh" w="100" justifyContent="center" alignItems="center">
+					<Box>
+						<Text>Sorry No Data Exists...</Text>
+					</Box>
+				</HStack>
+			)}
 		</>
 	);
 };
