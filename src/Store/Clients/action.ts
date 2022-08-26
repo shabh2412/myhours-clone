4e9188ef40dbb15e8694cd70fff6e44874abdc32
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-27 04:28:18 +0530

    created delete operation and added states for other actions

--- src/Store/Clients/action.ts
+++ src/Store/Clients/action.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { Dispatch } from "react";
 import {
+	ClientDeleteError,
+	ClientDeleteLoading,
+	ClientDeleteSuccess,
 	ClientDispatchHandler,
 	ClientPostError,
 	ClientPostLoading,
@@ -8,6 +11,9 @@ import {
 	ClientsGetError,
 	ClientsGetLoading,
 	ClientsGetSuccess,
+	CLIENTS_DELETE_ERROR,
+	CLIENTS_DELETE_LOADING,
+	CLIENTS_DELETE_SUCCESS,
 	CLIENTS_GET_ERROR,
 	CLIENTS_GET_LOADING,
 	CLIENTS_GET_SUCCESS,
@@ -62,6 +68,20 @@ const clientPostError = (value: boolean): ClientPostError => ({
 	type: CLIENT_POST_ERROR,
 	payload: value,
 });
+const clientDeleteLoading = (value: boolean): ClientDeleteLoading => ({
+	type: CLIENTS_DELETE_LOADING,
+	payload: value,
+});
+
+const clientDeleteSuccess = (payload: boolean): ClientDeleteSuccess => ({
+	type: CLIENTS_DELETE_SUCCESS,
+	payload,
+});
+
+const clientDeleteError = (value: boolean): ClientDeleteError => ({
+	type: CLIENTS_DELETE_ERROR,
+	payload: value,
+});
 
 export const postClient =
 	(data: ClientType): any =>
@@ -71,6 +91,8 @@ export const postClient =
 			.post(url, data)
 			.then(() => {
 				dispatch(clientPostSuccess(true));
+			})
+			.then(() => {
 				dispatch(getClients());
 			})
 			.catch(() => {
@@ -81,12 +103,17 @@ export const postClient =
 export const deleteClient =
 	(clientId: string): any =>
 	(dispatch: Dispatch<ClientDispatchHandler>) => {
+		dispatch(clientDeleteLoading(true));
 		axios
 			.delete(`${url}/${clientId}`)
+			.then(() => {
+				dispatch(clientDeleteSuccess(true));
+			})
 			.then(() => {
 				dispatch(getClients());
 			})
 			.catch((e) => {
+				dispatch(clientDeleteError(true));
 				console.log(e.message);
 			});
 	};
--- src/Store/Clients/types.ts
+++ src/Store/Clients/types.ts
@@ -17,6 +17,10 @@ export const CLIENTS_GET_LOADING = "CLIENTS_GET_LOADING";
 export const CLIENTS_GET_SUCCESS = "CLIENTS_GET_SUCESS";
 export const CLIENTS_GET_ERROR = "CLIENTS_GET_ERROR";
 
+export const CLIENTS_DELETE_LOADING = "CLIENTS_DELETE_LOADING";
+export const CLIENTS_DELETE_SUCCESS = "CLIENTS_DELETE_SUCESS";
+export const CLIENTS_DELETE_ERROR = "CLIENTS_DELETE_ERROR";
+
 export interface ClientsGetSuccess {
 	type: typeof CLIENTS_GET_SUCCESS;
 	payload: ClientType[];
@@ -46,10 +50,27 @@ export interface ClientPostError {
 	payload: boolean;
 }
 
+export interface ClientDeleteLoading {
+	type: typeof CLIENTS_DELETE_LOADING;
+	payload: boolean;
+}
+
+export interface ClientDeleteSuccess {
+	type: typeof CLIENTS_DELETE_SUCCESS;
+	payload: boolean;
+}
+export interface ClientDeleteError {
+	type: typeof CLIENTS_DELETE_ERROR;
+	payload: boolean;
+}
+
 export type ClientDispatchHandler =
 	| ClientsGetSuccess
 	| ClientsGetLoading
 	| ClientsGetError
 	| ClientPostLoading
 	| ClientPostError
-	| ClientPostSuccess;
+	| ClientPostSuccess
+	| ClientDeleteLoading
+	| ClientDeleteError
+	| ClientDeleteSuccess;
