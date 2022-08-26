fdd10c83fb683f8f490d59e1990a1958314e596e
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-26 17:02:22 +0530

    added client and team reducers

--- src/Store/store.tsx
+++ src/Store/store.tsx
@@ -1,18 +1,21 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
+import { clientReducer } from "./Clients/reducer";
 import { loginReducer } from "./Login/reducer";
 import { profileReducer } from "./Profile/reducer";
 import { registerReducer } from "./Register/reducer";
-
+import { teamReducer } from "./Team/reducer";
 
 const rootReducer = combineReducers({
-    register : registerReducer,
-    loginR : loginReducer,
-    profile : profileReducer
-})
+	register: registerReducer,
+	loginR: loginReducer,
+	profile: profileReducer,
+	clients: clientReducer,
+	teamMembers: teamReducer,
+});
 
-const middleware = applyMiddleware(thunk)
+const middleware = applyMiddleware(thunk);
 
-export const store = legacy_createStore(rootReducer,middleware)
+export const store = legacy_createStore(rootReducer, middleware);
 
-export type RootReducer = ReturnType<typeof rootReducer>
+export type RootReducer = ReturnType<typeof rootReducer>;
