b78d66e50d809658f0434315e6c5b8819cc44855
AnshGirdhar1 <anshgirdhar20@gmail.com>
2022-08-26 14:42:04 +0530

    Added TeamFormPage , Added Table Component

---
+++ src/Pages/Dashboard/Components/FormBillingOptions.tsx
@@ -0,0 +1,16 @@
+import { Heading, Input, Stack, Text } from "@chakra-ui/react";
+type pricing = {
+    heading : string;
+    inputChange : Function,
+    labour_rate? : string,
+    billing_rate? : string
+}
+export default function FormBillingOptions({heading,inputChange,labour_rate,billing_rate}:pricing){
+    return (
+        <Stack bg="#f9fafb" p="30px" spacing="20px" borderRadius="10px">
+                <Heading fontSize="25px" fontFamily="Poppins,sans-serif" fontWeight="500">{heading}</Heading>
+                <Text fontSize="13px">Enter default hourly rate for the team member to calculate Labor cost. This rate can be further specified on individual projects.</Text>
+                <Input name={heading==="Labour Rate" ? "labour_rate" : "billing_rate"} value={heading==="Labour Rate" ? labour_rate : billing_rate} onChange = {(e) => inputChange(e)} placeholder="0" bg="white"></Input>
+        </Stack>
+    )
+}
---
+++ src/Store/Team/reducer.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { ADD_TEAM_MEMBER, DELETE_TEAM_MEMBER, GET_TEAM_MEMBERS } from "./action.types";
+import { initialTeamState } from "../../Pages/Dashboard/TeamFormPage"
+import { action } from "./actions";
+
+const startingState:initialTeamState[] = [];
+
+export const teamReducer = (state:initialTeamState[]= startingState,{type,payload}:action) => {
+    switch(type){
+        case GET_TEAM_MEMBERS : {
+            console.log(payload);
+           return state;
+        } case ADD_TEAM_MEMBER : {
+            return state;
+        } case DELETE_TEAM_MEMBER : {
+            return state;
+        } default : {
+            return state;
+        }
+    }
+}
