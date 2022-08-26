7a8c1bf997ce5fee842f8d79f321bcc02127c970
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-26 18:51:26 +0530

    showing client data on client page

--- src/Pages/Dashboard/Clients.tsx
+++ src/Pages/Dashboard/Clients.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import SectionHeader from "../../Components/Dashboard/SectionHeaders/SectionHeader";
+import { getClients } from "../../Store/Clients/action";
+import { RootReducer } from "../../Store/store";
+import DataTable from "./Components/Table";
 
 type Props = {};
 
 const Clients = (props: Props) => {
+	const { data } = useSelector((state: RootReducer) => state.clients);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(getClients());
+	}, []);
+	useEffect(()=>{
+		console.log(data);
+	},[])
 	return (
 		<>
 			<SectionHeader title="Clients" buttonText="New Client" formLink="new" />
 			{/* Add table component after merging. */}
+			{data.length > 0 && <DataTable tableData={data} />}
 		</>
 	);
 };
