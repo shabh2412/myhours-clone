ce909a9659900489bf3e7fe6dd4c8c5caa81970c
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-26 14:29:42 +0530

    created client add form

---
+++ src/Components/Dashboard/Forms/ClientFormInput.tsx
@@ -0,0 +1,49 @@
+import { FormControl, FormLabel, Input, Textarea } from "@chakra-ui/react";
+import React from "react";
+
+type Props = {
+	name: string;
+	value: string;
+	changeHandler: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+	isTextArea?: boolean;
+};
+
+const ClientFormInput = ({
+	name,
+	value,
+	changeHandler,
+	isTextArea = false,
+}: Props) => {
+	return (
+		<FormControl my="4">
+			<FormLabel
+				fontSize="12px"
+				textTransform="uppercase"
+				fontWeight="bold"
+				color="gray.600"
+				mb="1">
+				{name}
+			</FormLabel>
+			{!isTextArea ? (
+				<Input
+					type="text"
+					name={name}
+					value={value}
+					onChange={(e) => {
+						changeHandler(e);
+					}}
+				/>
+			) : (
+				<Textarea
+					name={name}
+					value={value}
+					onChange={(e) => {
+						changeHandler(e);
+					}}
+				/>
+			)}
+		</FormControl>
+	);
+};
+
+export default ClientFormInput;
