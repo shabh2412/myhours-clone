d7c01d0282e88d8afde2d125bc07b6dae47109f7
Sneha052022 <sneha052020@gmail.com>
2022-08-27 22:32:50 +0530

    support page updated and datenavigator added

--- src/Pages/day1/ForSupport/ForSupportPage.tsx
+++ src/Pages/day1/ForSupport/ForSupportPage.tsx
@@ -26,45 +26,45 @@ import {
 function ForSupportPage() {
   return (
     <Box>
-      <Box marginTop={"150px"} marginLeft={"100px"}>
-        <Heading fontSize={"100px"} fontWeight={"light"}>
+      <Box marginTop={"50px"} marginLeft={"100px"}>
+        <Heading fontSize={"75px"} fontWeight={"light"}>
           We're here
         </Heading>
       </Box>
-      <Box marginTop={"80px"} marginLeft={"100px"}>
-        <Heading fontSize={"80px"} fontWeight={"medium"} color={"rgb(55 93 117)"}>
+      <Box marginTop={"60px"} marginLeft={"100px"}>
+        <Heading fontSize={"35px"} fontWeight={"medium"} color={"rgb(55 93 117)"}>
           We typically respond in less than 24 hours on business days. Contact
           us via email, live chat or the form below.
         </Heading>
       </Box>
-	  <Box marginTop={"100px"} marginLeft={"100px"}>
-        <Heading fontSize={"55px"} fontWeight={"medium"}>
+	  <Box marginTop={"30px"} marginLeft={"100px"}>
+        <Heading fontSize={"25px"} fontWeight={"medium"}>
 			
 		Read our help guides or send us a message to get detailed help.
         </Heading>
       </Box>
 	  <Box>
 	  <Flex justifyContent="center" marginTop={"100px"} >
-				<iframe style={{boxShadow:"1px 1px 15px #eeeeee"}} width="1200px" height="673px" src="https://www.youtube.com/embed/PqXO5AbUWpA" title="My Hours Intro video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+				<iframe style={{boxShadow:"1px 1px 15px #eeeeee"}} width="800px" height="453px" src="https://www.youtube.com/embed/PqXO5AbUWpA" title="My Hours Intro video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 			</Flex>
 			</Box>
-			<Divider  padding={"200px 100px 10px 100px"} color={"rgb(153 153 153)"} />
-			<Box marginLeft={"100px"} marginTop={"50px"}>
-				<Heading fontSize={"50px"} fontWeight={"medium"} color={"rgb(55 93 117)"}>What do you need help with?</Heading>
+			<Divider  padding={"90px 50px 10px 100px"} color={"rgb(153 153 153)"} />
+			<Box marginLeft={"100px"} marginTop={"30px"}>
+				<Heading fontSize={"35px"} fontWeight={"medium"} color={"rgb(55 93 117)"}>What do you need help with?</Heading>
 			</Box>
 			<Box padding={"20px 100px 20px 100px"}>
 				<Input  width={"100%"} placeholder="I have a question before signing up"></Input>
 			</Box>
 			<Box  padding={"5px 100px 10px 100px"}>
-			<Heading color ="gray" paddingBottom={"10px"}>Message</Heading>
+			<Heading color ="gray" paddingBottom={"10px"} fontSize={"20px"}>Message</Heading>
 				<Textarea placeholder="What's your issue or question"></Textarea>
 			</Box>
 			<Box  padding={"5px 100px 10px 100px"}>
-			<Heading color ="gray" paddingBottom={"10px"}>Name</Heading>
+			<Heading color ="gray" paddingBottom={"10px"} fontSize={"20px"}>Name</Heading>
 				<Input placeholder="Enter your name"></Input>
 			</Box>
 			<Box  padding={"5px 100px 10px 100px"}>
-				<Heading  color ="gray" paddingBottom={"10px"}>Email Address</Heading>
+				<Heading  color ="gray" paddingBottom={"10px"} fontSize={"20px"}>Email Address</Heading>
 				<Input placeholder="Enter your email address"></Input>
 			</Box>
 			<Box padding={"50px 100px 100px 100px"}>
