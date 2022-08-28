7af621653bcd0ada3a634fbc7631d9d181576cc8
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-28 14:44:32 +0530

    created project section

--- src/Pages/Dashboard/AddProject.tsx
+++ src/Pages/Dashboard/AddProject.tsx
@@ -1,157 +1,189 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { QuestionOutlineIcon } from "@chakra-ui/icons";
 import { useState } from "react";
 import {
-  Box,
-  Text,
-  Img,
-  Button,
-  Divider,
-  Flex,
-  Grid,
-  GridItem,
-  Heading,
-  Spacer,
-  Stack,
-  Select,
-  HStack,
-  Icon,
-  VStack,
-  Container,
-  List,
-  ListItem,
-  UnorderedList,
-  Input,
-  Checkbox,
-  Center,
-  Textarea,
+	Box,
+	Text,
+	Button,
+	Heading,
+	Stack,
+	Select,
+	HStack,
+	Container,
+	Input,
+	Checkbox,
+	FormControl,
+	FormLabel,
 } from "@chakra-ui/react";
-const initData = {
-  name: "",
-  client: "",
-  description: "",
-  difficulty: "none",
-  duration: "",
+import CustomFormInput from "../../Components/Dashboard/Forms/CustomFormInput";
+import { useDispatch, useSelector } from "react-redux";
+import { RootReducer } from "../../Store/store";
+import { getClients } from "../../Store/Clients/action";
+import { useNavigate } from "react-router-dom";
+import { get_members } from "../../Store/Team/actions";
+import { postProject } from "../../Store/Projects/actions";
+import { ProjectType } from "../../Store/Projects/type";
+
+const initData: ProjectType = {
+	name: "",
+	Client: "",
+	description: "",
+	difficulty: "none",
+	duration: "",
+	projectTeamMembers: [],
 };
 
 function AddProject() {
-  const [data, setData] = useState(initData);
+	const navigate = useNavigate();
+	const [data, setData] = useState(initData);
+	const { data: clients } = useSelector((state: RootReducer) => state.clients);
+	const teamMembers = useSelector((state: RootReducer) => state.teamMembers);
+	const dispatch = useDispatch();
 
-  const handleChange = (e: any) => {
-    const { value, name } = e.target;
-    setData({
-      ...data,
-      [name]: value,
-    });
-  };
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    console.log(data);
-  };
-  return (
-    <Box alignItems={"center"} justifyContent={"center"}>
-      <HStack marginTop={"20px"} marginLeft={"620px"}>
-        <Heading>Add project</Heading>
-        <QuestionOutlineIcon color={"#3b8fc2"} />
-      </HStack>
-      <Box
-        justifyContent={"center"}
-        alignItems={"center"}
-        width="300px"
-        marginTop={"30px"}
-        marginLeft={"620px"}
-      >
-        <Text>NAME</Text>
-        <Input
-          name="name"
-          width={"700px"}
-          alignItems={"center"}
-          placeholder="Enter name of the project..."
-          onChange={handleChange}
-          value={data.name}
-        ></Input>
-      </Box>
-      <Box marginTop={"20px"} marginLeft={"620px"}>
-        <Text>CLIENT</Text>
-        <Input
-          name="client"
-          width={"700px"}
-          alignItems={"center"}
-          placeholder="client..."
-          onChange={handleChange}
-          value={data.client}
-        ></Input>
-      </Box>
-      <Box
-        justifyContent={"center"}
-        alignItems={"center"}
-        width="300px"
-        marginTop={"30px"}
-        marginLeft={"620px"}
-      >
-        <Text>DESCRIPTION</Text>
-        <Textarea
-          name="description"
-          width={"700px"}
-          alignItems={"center"}
-          placeholder="Description..."
-          onChange={handleChange}
-          value={data.description}
-        ></Textarea>
-      </Box>
+	useEffect(() => {
+		dispatch(getClients());
+		dispatch(get_members());
+	}, []);
 
-      <Box
-        justifyContent={"center"}
-        alignItems={"center"}
-        width="300px"
-        marginTop={"30px"}
-        marginLeft={"620px"}
-      >
-        <Heading>Difficulty Level</Heading>
-        <Select
-          alignItems={"center"}
-          color={"#838688"}
-          name="difficulty"
-          onChange={handleChange}
-          value={data.difficulty}
-        >
-          <option value="none">Select Difficulty</option>
-          <option value="low">low</option>
-          <option value="high">high</option>
-        </Select>
-      </Box>
+	const handleTeamMemberToggle = (_id: string) => {
+		let members = data.projectTeamMembers;
+		if (members.find((memberId) => memberId === _id)) {
+			members = members.filter((id) => id !== _id);
+		} else {
+			members.push(_id);
+		}
+		setData((prev) => {
+			return {
+				...prev,
+				projectTeamMembers: members,
+			};
+		});
+	};
 
-      <Box
-        justifyContent={"center"}
-        alignItems={"center"}
-        width="300px"
-        marginTop={"30px"}
-        marginLeft={"620px"}
-      >
-        <Heading>Total Expected Duration</Heading>
-        <Input
-          name="duration"
-          width={"700px"}
-          alignItems={"center"}
-          placeholder="Total expected duration..."
-          onChange={handleChange}
-          value={data.duration}
-        ></Input>
-      </Box>
-      <HStack marginLeft={"620px"} marginTop={"30px"}>
-        <Button
-          onClick={handleSubmit}
-          backgroundColor={"#3b8fc2"}
-          color="white"
-        >
-          create project
-        </Button>
-        <Button backgroundColor={"white"} color="#3b8fc2" paddingLeft={"500px"}>
-          cancel
-        </Button>
-      </HStack>
-    </Box>
-  );
+	const handleChange = (
+		e: React.FormEvent<
+			HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+		>
+	) => {
+		const { value, name } = e.currentTarget;
+		setData({
+			...data,
+			[name]: value,
+		});
+	};
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+		dispatch(postProject(data));
+	};
+	return (
+		<Container my="10">
+			<HStack marginTop={"20px"} justifyContent="center">
+				<Heading>Add project</Heading>
+				<QuestionOutlineIcon color={"#3b8fc2"} />
+			</HStack>
+			<form>
+				<CustomFormInput
+					name="name"
+					changeHandler={handleChange}
+					value={data.name}
+				/>
+				<FormControl my="4">
+					<FormLabel
+						fontSize="12px"
+						textTransform="uppercase"
+						fontWeight="bold"
+						color="gray.600"
+						mb="1">
+						Client
+					</FormLabel>
+					<Select value={data.Client} name="Client" onChange={handleChange}>
+						<option>Select a client</option>
+						{clients.length > 0 &&
+							clients.map((client) => (
+								<option value={client._id} key={client._id}>
+									{client.name}
+								</option>
+							))}
+					</Select>
+				</FormControl>
+				<FormControl my="4">
+					<FormLabel
+						fontSize="12px"
+						textTransform="uppercase"
+						fontWeight="bold"
+						color="gray.600"
+						mb="1">
+						Select Team Members
+					</FormLabel>
+					<Stack direction="row" flexWrap="wrap">
+						{teamMembers.length > 0 &&
+							teamMembers.map((member) => (
+								<Checkbox
+									key={member._id}
+									onChange={() => {
+										handleTeamMemberToggle(member._id);
+									}}>
+									{member.name}
+								</Checkbox>
+							))}
+					</Stack>
+				</FormControl>
+				<CustomFormInput
+					name="description"
+					value={data.description}
+					changeHandler={handleChange}
+					isTextArea
+				/>
+				<Box bg="#f8fafc" my="4" p="4">
+					<Text fontSize="32px" fontWeight="500">
+						Difficulty Level
+					</Text>
+					<Select
+						alignItems={"center"}
+						color={"#838688"}
+						name="difficulty"
+						onChange={handleChange}
+						value={data.difficulty}>
+						<option value="none">Select Difficulty</option>
+						<option value="low">low</option>
+						<option value="high">high</option>
+					</Select>
+				</Box>
+				<Box bg="#f8fafc" my="4" p="4">
+					<Text fontSize="32px" fontWeight="500">
+						Total Expected Duration
+					</Text>
+					<Input
+						name="duration"
+						placeholder="Total expected duration..."
+						onChange={handleChange}
+						value={data.duration}></Input>
+				</Box>
+			</form>
+
+			<HStack my="4" justifyContent="start">
+				<Box>
+					<Button
+						onClick={handleSubmit}
+						bg="#3b8fc2"
+						variant="solid"
+						textTransform="capitalize"
+						color="white">
+						create project
+					</Button>
+				</Box>
+				<Button
+					backgroundColor={"white"}
+					textTransform="capitalize"
+					color="#3b8fc2"
+					onClick={() => {
+						navigate("/dashboard/projects");
+					}}>
+					cancel
+				</Button>
+			</HStack>
+		</Container>
+	);
 }
 
 export default AddProject;
--- src/Pages/Dashboard/Projects.tsx
+++ src/Pages/Dashboard/Projects.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import SectionHeader from "../../Components/Dashboard/SectionHeaders/SectionHeader";
+import { getProjects } from "../../Store/Projects/actions";
+import { RootReducer } from "../../Store/store";
 import DataTable from "./Components/Table";
 
 type Props = {};
@@ -8,15 +11,28 @@ const Projects = (props: Props) => {
 	const sQ = (name: string): any => {
 		console.log(name);
 	};
+	const { data } = useSelector((state: RootReducer) => state.projects);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getProjects());
+	}, []);
+
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
+
 	return (
 		<div>
 			<SectionHeader
 				title="Projects"
 				buttonText="Add new project"
 				formLink="new"
-				searchQ={sQ}
+				searchQ={getProjects}
 			/>
-			{/* <DataTable /> */}
+			{data.length > 0 && (
+				<DataTable tableData={data} deleteHandler={(name = "") => {}} />
+			)}
 		</div>
 	);
 };
